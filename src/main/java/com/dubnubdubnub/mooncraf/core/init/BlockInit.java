296d277ee5cfcbda183ea7ba1c18dc5daf10969f
dubnubdubnub <isaacchiu2006@gmail.com>
2021-03-11 19:12:05 -0700

    fix imports

--- src/main/java/com/dubnubdubnub/mooncraf/core/init/BlockInit.java
+++ src/main/java/com/dubnubdubnub/mooncraf/core/init/BlockInit.java
@@ -1,13 +1,13 @@
 package com.dubnubdubnub.mooncraf.core.init;
 
-import com.dubnubdubnub.mooncraf.mooncraf.Mooncraf;
+import com.dubnubdubnub.mooncraf.Mooncraf;
 
+import net.minecraft.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class BlockInit {
 	
-public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
-		Mooncraf.MOD_ID);
+	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Mooncraf.MOD_ID);
 
 }
