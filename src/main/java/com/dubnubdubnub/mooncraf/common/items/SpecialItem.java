fccf1dff58976a2640faa345333c00d23beb4505
dubnubdubnub <isaacchiu2006@gmail.com>
2021-03-11 18:40:24 -0700

    special item beginning

---
+++ src/main/java/com/dubnubdubnub/mooncraf/common/items/SpecialItem.java
@@ -0,0 +1,14 @@
+package com.dubnubdubnub.mooncraf.common.items;
+
+import net.minecraft.item.Item;
+
+public class SpecialItem extends Item{
+
+	public SpecialItem(Properties properties) {
+		super(properties);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
--- src/main/java/com/dubnubdubnub/mooncraf/core/init/ItemInit.java
+++ src/main/java/com/dubnubdubnub/mooncraf/core/init/ItemInit.java
@@ -1,6 +1,7 @@
 package com.dubnubdubnub.mooncraf.core.init;
 
 import com.dubnubdubnub.mooncraf.Mooncraf;
+import com.dubnubdubnub.mooncraf.common.items.SpecialItem;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -13,7 +14,13 @@ public class ItemInit {
 	
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Mooncraf.MOD_ID);
 	
-	public static final RegistryObject<Item> GRUEL = ITEMS.register("gruel", () -> new Item(new Item.Properties().group(ItemGroup.FOOD)));
+	
+	//Items
+	public static final RegistryObject<Item> GRUEL = ITEMS.register("gruel", 
+			() -> new Item(new Item.Properties().group(ItemGroup.FOOD)));
+	
+	public static final RegistryObject<SpecialItem> DEMONIC_SHARD = ITEMS.register("demonic shard", 
+			() -> new SpecialItem(new Item.Properties().group(ItemGroup.MISC)));
 	
 	
 	
