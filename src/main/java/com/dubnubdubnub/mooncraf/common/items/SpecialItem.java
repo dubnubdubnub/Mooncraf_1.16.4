24879beec46dcf72a04e96a6cf2a4a1d64dfe08c
dubnubdubnub <isaacchiu2006@gmail.com>
2021-03-11 21:01:31 -0700

    oya moar updateo

--- src/main/java/com/dubnubdubnub/mooncraf/common/items/SpecialItem.java
+++ src/main/java/com/dubnubdubnub/mooncraf/common/items/SpecialItem.java
@@ -7,9 +7,14 @@ import org.lwjgl.glfw.GLFW;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.client.util.InputMappings;
+import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -34,13 +39,29 @@ public class SpecialItem extends Item{
 		
 		if(InputMappings.isKeyDown(Minecraft.getInstance().getMainWindow().getHandle(),GLFW.GLFW_KEY_LEFT_SHIFT)) {
 			tooltip.add(new StringTextComponent("Rare. Powerful. Also Hurts. Seal Quickly, either into an artichoke or a branch"));
+		} else {
+			//"Hold " + "\u00A7e" + "Shift" + "\u00A77" + " for More Information"
+			tooltip.add(new TranslationTextComponent("tooltip.demonic_shard.hold_shift"));
+			
 		}
+	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+		// TODO Auto-generated method stub
+		playerIn.addPotionEffect(new EffectInstance(Effects.GLOWING, 200, 5));
+		ZombieEntity entity = new ZombieEntity(worldIn);
+		entity.setPosition(playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ());
+		worldIn.addEntity(entity);
+		return ActionResult.resultSuccess(playerIn.getHeldItem(handIn));
 	}
 	
 	@Override
 	public void onCreated(ItemStack stack, World worldIn, PlayerEntity playerIn) {
 		// TODO Auto-generated method stub
 		super.onCreated(stack, worldIn, playerIn);
+		
+		
 	}
 
 }
