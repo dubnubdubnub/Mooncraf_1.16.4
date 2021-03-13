832a12ba26d0f649554b224558520de103bbc3c9
onioncrab <60722212+onioncrab@users.noreply.github.com>
2021-03-12 21:05:57 -0700

    HEHEHEHEHEHESKYNET

--- src/main/java/com/dubnubdubnub/mooncraf/core/init/BlockInit.java
+++ src/main/java/com/dubnubdubnub/mooncraf/core/init/BlockInit.java
@@ -9,5 +9,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class BlockInit {
 	
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Mooncraf.MOD_ID);
+	
+	
+	public static final RegistryObject<Block> UNAWAKENED_QUALIAC_STONE = BLOCKS.register ("Unawakened_Qualic_Stone, 
+			() -> new Block(AbstractBlock.Properties.create(Material.OBSIDIAN, MaterialColor.RED)
+					.hardnessAndResitance(50f, 3000f)
+				        .harvestTool(ToolType.HOE)
+				        .harvestLevel(3)));
+					
 
 }
