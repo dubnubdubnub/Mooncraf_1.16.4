dddaa549cea3077c07ae2833ea725aee5bd27d88
dubnubdubnub <isaacchiu2006@gmail.com>
2021-03-19 22:14:51 -0600

    asdsadsadsadas

--- src/main/java/com/dubnubdubnub/mooncraf/core/init/BlockInit.java
+++ src/main/java/com/dubnubdubnub/mooncraf/core/init/BlockInit.java
@@ -21,6 +21,28 @@ public class BlockInit {
 						.hardnessAndResistance(50f, 3000f)
 				        .harvestTool(ToolType.HOE)
 				        .harvestLevel(3)));
+	
+	
+	/*
+	 * 
+	 * 
+{
+    "parent": "block/cube", 
+    "textures": {
+      "top": "mooncraf:blocks/qstone_top_inactive",
+      "front": "mooncraf:blocks/qstone_front_inactive",
+      "side": "mooncraf:blocks/qstone_side_inactive"
+    }
+}
+
+
+
+
+
+
+	 * 
+	 * 
+	 * */
 					
 
 }
