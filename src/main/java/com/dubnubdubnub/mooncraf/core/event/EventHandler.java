aae25dd0909bd381a22307aafa3a818771265446
dubnubdubnub <isaacchiu2006@gmail.com>
2021-05-21 12:15:36 -0600

    Update EventHandler.java

--- src/main/java/com/dubnubdubnub/mooncraf/core/event/EventHandler.java
+++ src/main/java/com/dubnubdubnub/mooncraf/core/event/EventHandler.java
@@ -1,6 +1,7 @@
 package com.dubnubdubnub.mooncraf.core.event;
 
 import com.dubnubdubnub.mooncraf.Mooncraf;
+
 import com.dubnubdubnub.mooncraf.core.init.BlockInit;
 
 import net.minecraft.block.AnvilBlock;
@@ -15,16 +16,13 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 @EventBusSubscriber(modid = Mooncraf.MOD_ID, bus = Bus.FORGE)
 public class EventHandler {
 
-	/*
-	 * @SubscribeEvent public static void onBlockBreak(final BlockEvent.BreakEvent
-	 * event) { if(event.getState().getBlock() instanceof AnvilBlock &&
-	 * event.getPlayer().isAirBorne == true) { for(int x = 0; x < 5; x++) { for(int
-	 * y = 0; y < 5; y++) { for(int z = 0; z < 5; z++) {
-	 * event.getWorld().setBlockState(event.getPos().add(x,y,z),
-	 * BlockInit.UNAWAKENED_QUALIC_STONE.get().getDefaultState().with(CustomBlock.
-	 * HORIZONTAL_FACING,
-	 * Direction.Plane.HORIZONTAL.random(event.getWorld().getRa)),
-	 * Constants.BlockFlags.BLOCK_UPDATE); } } } } }
-	 */
+	
+	  @SubscribeEvent public static void onBlockBreak(final BlockEvent.BreakEvent
+	  event) { if(event.getState().getBlock() instanceof AnvilBlock &&
+	  event.getPlayer().isAirBorne == true) { for(int x = 0; x < 5; x++) { for(int
+	  y = 0; y < 5; y++) { for(int z = 0; z < 5; z++) {
+	  event.getWorld().setBlockState(event.getPos().add(x,y,z),
+	  BlockInit.UNAWAKENED_QUALIC_STONE.get().getDefaultState().with(CustomBlock.HORIZONTAL_FACING, Direction.Plane.HORIZONTAL.random(event.getWorld().getRandom())), Constants.BlockFlags.BLOCK_UPDATE); } } } } }
+	 
 
 }
